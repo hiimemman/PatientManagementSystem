2f76367cae5f4f283cecf7b6f5e8240fdaf74d3c
Emmanuel Nocum <emmanuel.nocum@cvsu.edu.ph>
2023-09-09 20:02:30 +0800

    First commit

---
+++ PatientManagementSystem.xaml.cs
@@ -0,0 +1,44 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PatientManagementSystem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+
+        private LogIn loginPage;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+ 
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            // Navigate to the login page
+            LogIn loginPage = new LogIn();
+            //contentControl.Content = loginPage;
+
+            //btnLogin.Visibility = Visibility.Collapsed;
+        }
+    }
+}
